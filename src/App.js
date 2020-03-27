20ac9c933e0dfd7e2f73be2a3e3d3d1ef8e63052
Wesley Oliveira <fcowesleyosantos@gmail.com>
2020-03-27 11:49:56 -0300

    Start github versioning

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+
+//Custom StyleSheet
+import './App.css'
+
+//Components
+import Quiz from './components/Quiz'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <div className="d-flex justify-content-center mt-5">
+      <Quiz />
+    </div >
+  )
 }
 
-export default App;
+export default App
